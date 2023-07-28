d0891996e664ac75e19ca7ed2e062625c343867e
Батсүх Мөнхцог <HP233131@M301-CC0123.local>
2023-07-28 15:35:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
+import {useEffect, useRef, useState} from "react"
+import {useInterval} from "use-interval"
 
 function App() {
+  //maps and units
+  const map=useRef();
+  const [mapWidth,setMapWidth]=useState(null);
+  const [scale,setScale]=useState(10);
+  const [smallestUnit,setSmallestUnit]=useState(null);
+  //time
+  const [time,setTime]=useState(500);
+  //players
+  const [player1,setPlayer1]=useState([{top: 0, left: 0}]);
+  const [player2,setPlayer2]=useState([{top: 0, left: 0}]);
+  const [direction1,setDirection1]=useState("right");
+  const [direction2,setDirection2]=useState("left");
+
+  useEffect(()=>{
+    setMapWidth(map.current.offsetWidth);
+  },[]);
+
+  useEffect(()=>{
+    setSmallestUnit(mapWidth/scale);
+  },[mapWidth]);
+
+  useEffect(()=>{
+    const newPlayer2 = player2.map((player2El,i)=> {
+      if(i==0){
+       return {top:0 , left:mapWidth-smallestUnit}
+      }
+     });
+     setPlayer2(newPlayer2);
+  },[smallestUnit]);
+
+  function player1Function(){
+    if(direction1=="right" && player1[0].left+2*smallestUnit<=mapWidth){
+      const newPlayer1 = player1.map((player1El,i)=> {
+        if(i==0){
+         return {top:0 , left:player1El.left+smallestUnit}
+        }
+      });
+      setPlayer1(newPlayer1);
+    }
+    // if(direction1=="right" && player1[0].left+2*smallestUnit<=mapWidth){
+    //   const newPlayer1 = player1.map((player1El,i)=> {
+    //     if(i==0){
+    //      return {top:0 , left:player1El.left+smallestUnit}
+    //     }
+    //   });
+    //   setPlayer1(newPlayer1);
+    // }
+    // if(direction1=="right" && player1[0].left+2*smallestUnit<=mapWidth){
+    //   const newPlayer1 = player1.map((player1El,i)=> {
+    //     if(i==0){
+    //      return {top:0 , left:player1El.left+smallestUnit}
+    //     }
+    //   });
+    //   setPlayer1(newPlayer1);
+    // }
+    // if(direction1=="right" && player1[0].left+2*smallestUnit<=mapWidth){
+    //   const newPlayer1 = player1.map((player1El,i)=> {
+    //     if(i==0){
+    //      return {top:0 , left:player1El.left+smallestUnit}
+    //     }
+    //   });
+    //   setPlayer1(newPlayer1);
+    // }
+  }
+
+  window.addEventListener("keydown",(e)=>{
+    // switch(e.key){
+    //   case:""
+    // }
+  })
+
+  useInterval(()=>{
+    player1Function()
+  },[time])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.screen}>
+      <div ref={map} className={style.map}>
+        {
+          player1?.map((player1El,i)=>{
+            return(
+              <div
+                style={{
+                  position:"absolute",
+                  width:smallestUnit,
+                  height:smallestUnit,
+                  backgroundColor:"blue",
+                  top:player1El.top,
+                  left:player1El.left
+                }}
+                key={i}
+              >
+              </div>
+            )
+          })
+        }
+        {
+          player2?.map((player2El,i)=>{
+            return(
+              <div
+                style={{
+                  position:"absolute",
+                  width:smallestUnit,
+                  height:smallestUnit,
+                  backgroundColor:"red",
+                  top:player2El.top,
+                  left:player2El.left
+                }}
+                key={i}
+              >
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
